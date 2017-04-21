38a834f449291f5785922d1f220c7da8460a7566
Josh <joshmvince@gmail.com>
2017-04-21 21:15:13 +0100

    add tests for scraper

--- test/scraperTest.js
+++ test/scraperTest.js
@@ -1,11 +1,26 @@
-var assert = require('chai').assert;
-var should = require('chai').should();
-var expect = require('chai').expect;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var assert = chai.assert;
+var should = chai.should();
+chai.config.includeStack = true;
+
 var Scraper = require('../src/scraper.js');
 var testHtml = require('./testHtml.js');
 
 describe('Scraper', function() {
-  describe('scrape', function () {
+  
+  describe('request', function() {
+    var url = "http://www.skysports.com/football/transfer-paper-talk"
+    it('returns an html string from the webpage', function() {
+      var value = Scraper.request(url)
+      expect(value).to.eventually.be.a('string')
+    });
+    
+  });
+  
+  describe('scrape', function() {
     var scrapedObj = Scraper.scrape(testHtml);
     it('should return an array of story strings', function(){
       scrapedObj.should.have.property('stories')
