165e4dbb36d79d4b8b3a6507250b1cd50ec1217d
Josh <joshmvince@gmail.com>
2017-04-23 13:09:37 +0100

    scraper now fetches html and grabs stories in one function - adds tests for this too

--- src/scraper.js
+++ src/scraper.js
@@ -1,32 +1,47 @@
 const cheerio = require('cheerio');
 const rp = require('request-promise');
 
-function request(url) {
-  return rp(url)
-    .then(function(str){
-    return str
-    })
-    .catch(function(err){
+function getHtml(url) {
+  return new Promise((resolve, reject) => {
+    rp(url).then(str => {
+      resolve(str)
+    }).catch(err => {
       console.error(err)
+      reject(err)
     })
+  });
 }
 
-function scrape(html){
-  const $ = cheerio.load(html)
+// find Story text from the html string fetched from sky sports
 
-  function getStories(containerElement) {
-    var stories = $(containerElement).find($('li')).map(function (i,e) {
-      return $(this).text()
-    }).get()
-    return {stories: sanitize(stories)}
-  }
-
-  function sanitize(arr) {
-    return arr.map(function(str) {
-      return str.trim().replace('&', 'and');
-    })
-  }
-  return getStories('.paper-stories')
+function getStoryText(htmlString, className) {
+  return findListItemsText(htmlString, className).map(str => {
+    return sanitizeString(str);
+  })
 }
 
-module.exports = {request: request, scrape: scrape}
+function findListItemsText(dataString, className) {
+  const $ = cheerio.load(dataString);
+  var listItemText = $(className).find($('li')).map(function (i,e) {
+    return $(this).text()
+  }).get()
+  return listItemText
+}
+
+function sanitizeString(str) {
+  return str.trim().replace('&', 'and');
+}
+
+function scrape(url, className) {
+  return getHtml(url).then(dataStr => {
+    var stories = getStoryText(dataStr, className)
+    console.log(stories)
+    return {stories: stories}
+  })
+}
+
+module.exports = {
+  getHtml: getHtml, 
+  getStoryText: getStoryText,
+  scrape: scrape
+}
--- test/scraperTest.js
+++ test/scraperTest.js
@@ -11,22 +11,37 @@ var testHtml = require('./testHtml.js');
 
 describe('Scraper', function() {
   
-  describe('request', function() {
+  describe('getHtml', function() {
     var url = "http://www.skysports.com/football/transfer-paper-talk"
-    it('returns an html string from the webpage', function() {
-      var value = Scraper.request(url)
-      expect(value).to.eventually.be.a('string')
+    it('eventually returns an html string from the webpage', function() {
+      var value = Scraper.getHtml(url)
+      return expect(value).to.eventually.be.a('string')
     });
-    
+  });
+
+  describe('getStoryText', function () {
+    it('grabs text from HTML and returns an array of sanitized strings', function () {
+      var stories = Scraper.getStoryText(testHtml, '.paper-stories');
+      stories.should.be.an('array')
+      stories.forEach(el => {
+        el.should.be.a('string')
+      })
+    });
+    it('returns an empty array if there were no stories in the html', function () {
+      var stories = Scraper.getStoryText('<html>nothing here</html>', '.paper-stories');
+      stories.should.be.an('array').with.length(0)
+    });
   });
   
-  describe('scrape', function() {
-    var scrapedObj = Scraper.scrape(testHtml);
-    it('should return an array of story strings', function(){
-      scrapedObj.should.have.property('stories')
-      scrapedObj.stories.forEach(function(el){
-        assert.isString(el);
-      })      
+  describe('scrape', function () {
+    var url = "http://www.skysports.com/football/transfer-paper-talk"
+    var object = Scraper.scrape(url, '.paper-stories')
+    
+    it('returns an object with an array of stories in it', function () {
+      return expect(object).to.eventually.be.an('object').with.property('stories')
+      return expect(object.stories).to.eventually.be.an('array').forEach(str => {
+        str.should.be.a('string')
+      })
     });
   });
   
